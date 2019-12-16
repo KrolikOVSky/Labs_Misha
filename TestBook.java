1412f179c5a5339d003c3eec388321e8df42a30a
MikoShark <maikl.akulov@yandex.ru>
2019-12-16 18:32:36 +0300

    COMMENTED

--- TestBook.java
+++ TestBook.java
@@ -8,11 +8,11 @@ public class TestBook
         Book book4 =  new  Book(144, "leg", "Dar", "hop", 2212, 23, 3, 66);
         Book book5 = null;
         //kigffyuofhyjhgjfhijlh
-        System.out.printf("Книга №2\n%s\n",book2.toString());
-        System.out.printf("Книга №3\n%s\n",book3.toString());
+        // System.out.printf("Книга №2\n%s\n",book2.toString());
+        // System.out.printf("Книга №3\n%s\n",book3.toString());
         System.out.printf("Книга №4\n%s\n",book4.toString());
         System.out.println("b5=" + book5);
-        System.out.println("b1==b2: " + book1.equals(book2));
+        // System.out.println("b1==b2: " + book1.equals(book2));
         System.out.println("b1==b3: " + book1.equals(book3));
         System.out.println("b1==b4: " + book1.equals(book4));
         System.out.println("b2==b3: " + book2.equals(book3));
